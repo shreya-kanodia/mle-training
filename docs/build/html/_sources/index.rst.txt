4e22f235b3d8591bc67540de4447eff3c43c3cc9
shreya-kanodia <shreya.kanodia@tigeranalytics.com>
2022-05-06 20:26:42 +0530

    Assignment 4.1

---
+++ docs/build/html/_sources/index.rst.txt
@@ -0,0 +1,20 @@
+.. Hosuing_Price documentation master file, created by
+   sphinx-quickstart on Fri May  6 20:11:10 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Hosuing_Price's documentation!
+=========================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+# , encoding="utf-8"
+setuptools.setup(
+    name="housing_price",
+    version="0.3",
+    author="Shreya Kanodia",
+    author_email="shreya.kanodia@tigeranalytics.com",
+    description="Package made for assignment 4.1",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shreya-kanodia/mle-training",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
